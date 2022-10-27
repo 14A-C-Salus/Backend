88f82bb8fa0a7354a4e4850c59740946af36ddd0
VargaBalazsAdam <v.balazs.adam@gmail.com>
2022-10-27 11:17:45 +0100

    Auth files

---
+++ Authentication/Controllers/Models/AuthLoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Authentication.Controllers.Models
+{
+    public class AuthLoginRequest
+    {
+        [Required, MinLength(8, ErrorMessage = "Please enter at least 8 characters!"), MaxLength(20, ErrorMessage = "The username may not be longer than 20 characters.")]
+        public string username { get; set; } = string.Empty;
+
+        [Required, MinLength(8, ErrorMessage = "Please enter at least 8 characters!"), MaxLength(20, ErrorMessage = "The password may not be longer than 20 characters.")]
+        public string password { get; set; } = string.Empty;
+    }
+}
