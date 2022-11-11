ce2135459893a13cd120a41f6b5121d6ea01db66
VargaBalazsAdam <v.balazs.adam@gmail.com>
2022-11-11 16:33:57 +0100

    Fix the email body

--- Authentication/Services/AuthServices/AuthService.cs
+++ Authentication/Services/AuthServices/AuthService.cs
@@ -28,110 +28,73 @@ namespace Authentication.Services.AuthServices
             string password = _configuration.GetSection("MailSettings:Password").Value;
             string subject = "Verify your account";
             string url = $"https://salus.bsite.net/api/Auth/verify?token={auth.verificationToken}";
+            string imgUrl = "https://i.ibb.co/Mcz2mRc/logo.png";
             if (_configuration.GetSection("Host:IsLocalHost").Value == "Yes")
                 url = $"https://localhost:7138/api/Auth/verify?token={auth.verificationToken}";
-            string body = $"<!DOCTYPE html>\r\n" +
-                $"<html lang=\"en\">\r\n" +
-                $"<head>\r\n" +
-                $"    <meta charset=\"UTF-8\">\r\n" +
-                $"    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n" +
-                $"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" +
-                $"    <title>Verify</title>\r\n" +
-                $"</head>\r\n<style>\r\n" +
-                $"    body{{\r\n" +
-                $"        background-color:#01a36269;\r\n" +
-                $"        height: 100vw;\r\n" +
-                $"        scroll-behavior: smooth;\r\n" +
-                $"    }}\r\n" +
-                $"    #main{{\r\n" +
-                $"        margin-left: 20%;\r\n" +
-                $"        background-color: rgb(255, 255, 255);\r\n" +
-                $"        width: 60%;\r\n" +
-                $"        padding-bottom: 2vw;\r\n" +
-                $"    }}\r\n" +
-                $"    #logo{{\r\n" +
-                $"        width: 30vw;\r\n" +
-                $"        margin-left: 15vw;\r\n" +
-                $"    }}\r\n" +
-                $"    #click{{\r\n " +
-                $"       text-align: center;\r\n" +
-                $"        font-size: 4vw;\r\n" +
-                $"        color:#01a362;\r\n " +
-                $"       font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;\r\n" +
-                $"    }}\r\n" +
-                $"    \r\n" +
-                $"#button {{\r\n" +
-                $"    font-family: Georgia, 'Times New Roman', Times, serif;\r\n" +
-                $"  align-items: center;\r\n" +
-                $"  background-image: linear-gradient(144deg,#01a362, #00cf7c 50%,#01a362);\r\n" +
-                $"  border: 0;\r\n" +
-                $"  border-radius: 20px;\r\n" +
-                $"  color: #ffffff;\r\n" +
-                $"  display: flex;\r\n" +
-                $"  font-size: 2.5vw;\r\n" +
-                $"  justify-content: center;\r\n" +
-                $"  padding: 3px;\r\n" +
-                $"  cursor: pointer;\r\n" +
-                $"  margin-top: 5vw;\r\n" +
-                $"  margin-left: 10vw;\r\n" +
-                $"  width: 30vw;\r\n" +
-                $"}}\r\n" +
-                $"\r\n" +
-                $"#button span {{\r\n" +
-                $"  background-color: #005734;\r\n" +
-                $"  padding: 16px 24px;\r\n" +
-                $"  border-radius: 20px;\r\n" +
-                $"  width: 100%;\r\n" +
-                $"  height: 3vw;\r\n" +
-                $"}}\r\n" +
-                $"\r\n" +
-                $"#button:hover span {{\r\n" +
-                $"  background: none;\r\n" +
-                $"}}\r\n" +
-                $"#body{{\r\n" +
-                $"    margin: 5vw;\r\n" +
-                $"}}\r\n" +
-                $"#hi{{\r\n" +
-                $"    font-size: 3.5vw;\r\n" +
-                $"    text-align: center;\r\n" +
-                $"}}\r\n" +
-                $".main-text-size{{\r\n" +
-                $"    font-size: 2.5vw;\r\n" +
-                $"}}\r\n" +
-                $"footer{{\r\n" +
-                $"    background-color: #005734;\r\n" +
-                $"    height: 15vw;\r\n" +
-                $"    margin-left: 20%;\r\n" +
-                $"    width: 60%;\r\n" +
-                $"}}\r\n" +
-                $"</style>\r\n" +
-                $"<body>\r\n" +
-                $"    <div id=\"main\">\r\n" +
-                $"        <img id=\"logo\" src=\"images/logo.png\" alt=\"Logo\">\r\n" +
-                $"        <h1 id=\"click\">Click the button, </br>\r\n" +
-                $"            to verify your account!</h1>\r\n" +
-                $"        <div id=\"body\">\r\n" +
-                $"            <h2 id=\"hi\">Hi {auth.username}!</h2>\r\n" +
-                $"            <p class=\"main-text-size\">\r\n" +
+            string body = 
+                $"<div style=\"background-color:#01a36269;\r\n" +
+                $"            height: 100%;\r\n" +
+                $"            scroll-behavior: smooth;\">\r\n" +
+                $"    <div style=\"margin-left: 20%;\r\n" +
+                $"                background-color: rgb(255, 255, 255);\r\n" +
+                $"                width: 60%;\r\n" +
+                $"                padding-bottom: 2vw;\">\r\n" +
+                $"        <img style=\"width: 50%;\r\n" +
+                $"                    margin-left: 25%;\"\r\n" +
+                $"                    src='{imgUrl}' alt=\"Logo\">\r\n" +
+                $"        <h1 style=\"text-align: center;\r\n" +
+                $"                    font-size: 4vw;\r\n" +
+                $"                    color:#01a362;\r\n" +
+                $"                    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;\">\r\n" +
+                $"                    Click the button, </br>\r\n" +
+                $"                    to verify your account!\r\n" +
+                $"        </h1>\r\n" +
+                $"        <div style=\"margin: 5vw;\">\r\n" +
+                $"            <h2 style=\"font-size: 3.5vw;\r\n" +
+                $"                        text-align: center;\">\r\n" +
+                $"                        Hi {auth.username}!\r\n" +
+                $"            </h2>\r\n" +
+                $"            <p style=\"font-size: 2.5vw;\">\r\n" +
                 $"                Thanks for using Salus - Healthy lifestyle!\r\n" +
                 $"                Please click on the button below to confirm your email address.\r\n" +
                 $"            </p>\r\n" +
-                $"            <strong class=\"main-text-size\">After the verification process is complete:</strong>\r\n" +
+                $"            <strong style=\"font-size: 2.5vw;\">After the verification process is complete:</strong>\r\n" +
                 $"            <ul>\r\n" +
-                $"                <li class=\"main-text-size\">\r\n" +
-                $"                    Please set your personal information! \r\n" +
+                $"                <li style=\"font-size: 2.5vw;\">\r\n" +
+                $"                    Please set your personal information!\r\n" +
                 $"                </li>\r\n" +
-                $"                <li class=\"main-text-size\">\r\n" +
+                $"                <li style=\"font-size: 2.5vw;\">\r\n" +
                 $"                    Enjoy our app!\r\n" +
                 $"                </li>\r\n" +
                 $"            </ul>\r\n" +
                 $"            <form name=\"myForm\" target=\"_blank\" action='{url}' method=\"post\">\r\n" +
-                $"                <button id=\"button\" type=\"submit\" name=\"submit_param\" value=\"submit_value\"><span class=\"text\">VERIFY</span></button>\r\n" +
+                $"                <button style=\"font-family: Georgia, 'Times New Roman', Times, serif;\r\n" +
+                $"                            align-items: center;\r\n" +
+                $"                            background-image: linear-gradient(144deg,#01a362, #00cf7c 50%,#01a362);\r\n" +
+                $"                            border: 0;\r\n" +
+                $"                            border-radius: 20px;\r\n" +
+                $"                            color: #ffffff;\r\n" +
+                $"                            display: flex;\r\n" +
+                $"                            font-size: 2.5vw;\r\n" +
+                $"                            justify-content: center;\r\n" +
+                $"                            padding: 3px;\r\n" +
+                $"                            cursor: pointer;\r\n" +
+                $"                            margin-top: 5vw;\r\n" +
+                $"                            width: 80%;\r\n" +
+                $"                            margin-left: 10%;\" type=\"submit\" name=\"submit_param\" value=\"submit_value\">\r\n" +
+                $"                            <span class=\"text\"\r\n" +
+                $"                                  style=\"background-color: #005734;\r\n" +
+                $"                                  padding: 16px 24px;\r\n" +
+                $"                                  border-radius: 20px;\r\n " +
+                $"                                  width: 100%;\r\n" +
+                $"                                  height: 3vw;\">\r\n" +
+                $"                                  VERIFY\r\n" +
+                $"                            </span>\r\n" +
+                $"                </button>\r\n" +
                 $"            </form>\r\n" +
                 $"        </div>\r\n" +
                 $"    </div>\r\n" +
-                $"</body>\r\n" +
-                $"</html>";
+                $"</div>";
 
             SmtpClient client = new SmtpClient
             {
