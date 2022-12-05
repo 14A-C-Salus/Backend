28d78caae78b498d08cc919391681f53d6b9b668
VargaBalazsAdam <v.balazs.adam@gmail.com>
2022-12-05 11:33:06 +0100

    Add delete comment controller and fix some code

--- Salus/Controllers/SocialMediaController.cs
+++ Salus/Controllers/SocialMediaController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Salus.Controllers.Models.AuthModels;
 
 namespace Salus.Controllers
 {
@@ -24,28 +23,27 @@ namespace Salus.Controllers
             if (followedAuth == null)
                 return BadRequest("Auth to follow doesn't exist.");
 
-            var followerUserProfile = await _dataContext.userProfiles.FirstOrDefaultAsync(u => u.authOfProfileId == followerAuth.id);
-            if (followerUserProfile == null)
+            if (followerAuth.userProfile == null)
                 return BadRequest("You need to create a user profile first!");
 
-            var followedUserProfile = await _dataContext.userProfiles.FirstOrDefaultAsync(u => u.authOfProfileId == followedAuth.id);
-            if (followedUserProfile == null)
+            if (followedAuth.userProfile == null)
                 return BadRequest($"{followerAuth.username} has no user profile!");
 
 
             string startStop;
-            if (await _dataContext.followings.FirstOrDefaultAsync(f => f.followerId == followerUserProfile.id && f.followedId == followedUserProfile.id) != null)
+            if (await _dataContext.followings
+                .FirstOrDefaultAsync(f => f.followerId == followerAuth.userProfile.id && f.followedId == followedAuth.userProfile.id) != null)
             {
                 _dataContext.followings.Remove(await _dataContext.followings.FirstAsync(
-                    f => f.followerId == followerUserProfile.id && f.followedId == followedUserProfile.id));
+                    f => f.followerId == followerAuth.userProfile.id && f.followedId == followedAuth.userProfile.id));
                 startStop = "stopped";
             }
             else
             {
                 _dataContext.followings.Add(new Following
                 {
-                    followed = followedUserProfile,
-                    follower = followerUserProfile,
+                    followed = followedAuth.userProfile,
+                    follower = followerAuth.userProfile,
                     followDate = DateTime.Now.ToString("yyyy.MM.dd")
                 });
                 startStop = "started";
@@ -64,18 +62,16 @@ namespace Salus.Controllers
             if (toAuth == null)
                 return BadRequest("'toAuth' doesn't exist.");
 
-            var writerUserProfile = await _dataContext.userProfiles.FirstOrDefaultAsync(u => u.authOfProfileId == writerAuth.id);
-            if (writerUserProfile == null)
+            if (writerAuth.userProfile == null)
                 return BadRequest("You need to create a user profile first!");
 
-            var toUserProfile = await _dataContext.userProfiles.FirstOrDefaultAsync(u => u.authOfProfileId == toAuth.id);
-            if (toUserProfile == null)
+            if (toAuth.userProfile == null)
                 return BadRequest($"{toAuth.username} has no user profile!");
 
             var comment = new Comment
             {
-                commentFrom = writerUserProfile,
-                commentTo = toUserProfile,
+                commentFrom = writerAuth.userProfile,
+                commentTo = toAuth.userProfile,
                 body = request.body,
                 sendDate = DateTime.Now.ToString("yyyy.MM.dd")
             };
@@ -83,6 +79,26 @@ namespace Salus.Controllers
             _dataContext.comments.Add(comment);
             await _dataContext.SaveChangesAsync();
             return Ok($"{writerAuth.username} sended a comment to {toAuth.username}!");
+        }
+
+        [HttpPost("delete-comment"), Authorize]
+        public async Task<IActionResult> DeleteComment(int commentId)
+        {
+            var auth = await _dataContext.auths.FirstAsync(a => a.email == _authService.GetEmail());
+
+            if (auth.userProfile == null)
+                return BadRequest("You need to create a user profile first!");
+
+            var comment = await _dataContext.comments.FirstOrDefaultAsync(c => c.id == commentId);
+
+            if (comment == null)
+                return BadRequest("Comment doesn't exist.");
+
+            if (auth.userProfile.id != comment.toId)
+                return BadRequest("You do not have the right to delete the comment.");
+            _dataContext.comments.Remove(comment);
+            await _dataContext.SaveChangesAsync();
+            return Ok($"{comment.id} deleted by {auth.username}!");
         }
     }
 }
