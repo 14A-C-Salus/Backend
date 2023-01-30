66dbb8ba4adabdde3e93d843ae5d8bc10579f78a
VargaBalazsAdam <v.balazs.adam@gmail.com>
2023-01-30 12:18:17 +0100

    tag update id

--- Salus/Controllers/Models/TagModels/TagUpdateRequest.cs
+++ Salus/Controllers/Models/TagModels/TagUpdateRequest.cs
@@ -4,6 +4,7 @@ namespace Salus.Controllers.Models.TagModels
 {
     public class TagUpdateRequest
     {
+        public int id { get; set; }
         public string name { get; set; } = string.Empty;
         public string description { get; set; } = string.Empty;
         public foodPropertiesEnum property { get; set; }
