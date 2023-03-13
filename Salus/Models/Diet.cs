7428720935ddb77a8c19b29d800919d58732f32c
Varga Balázs Ádám <112722847+VargaBalazsAdam@users.noreply.github.com>
2023-03-13 12:00:55 +0100

    try

--- Salus/Models/Diet.cs
+++ Salus/Models/Diet.cs
@@ -5,7 +5,10 @@
         public int id { get; set; }
         public string name { get; set; } = string.Empty;
         public string description { get; set; } = string.Empty;
-        public int maxKcal { get; set; }
+        public Between<int>? fat { get; set; }
+        public Between<int>? carbohydrate { get; set; }
+        public Between<int>? protein { get; set; }
+        public Between<int>? kcal { get; set; }
         public int minDl { get; set; }
         public virtual IList<UserProfile>? userProfiles { get; set; }
     }
