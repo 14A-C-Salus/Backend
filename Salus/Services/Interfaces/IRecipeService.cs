89e3c644a44f3a656e6368993d7fead5279e1058
Varga Balázs Ádám <112722847+VargaBalazsAdam@users.noreply.github.com>
2023-04-19 14:15:50 +0200

    clean code 1.1

--- Salus/Services/Interfaces/IRecipeService.cs
+++ Salus/Services/Interfaces/IRecipeService.cs
@@ -2,16 +2,16 @@
 {
     public interface IRecipeService
     {
-        List<Recipe> GetAll(int authId);
+        void Delete(int recipeId);
         Recipe Create(WriteRecipeRequest request);
         Recipe Update(UpdateRecipeRequest request);
-        void Delete(int recipeId);
         Recipe CreateSimple(RecipeCreateRequest request);
         Recipe UpdateSimple(RecipeUpdateRequest request);
         Recipe VerifyUnVerify(int id);
         Recipe AddTags(AddTagsToRecipeRequest request);
         List<Tag> GetRecommendedTags(int recipeId);
         List<UsersLikeRecipes> LikeUnlike(int recipeId);
+        List<Recipe> GetAll(int authId);
         List<Recipe> GetAllByTagId(int tagId);
     }
 }
